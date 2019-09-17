7ec7f12a5bab661fc2d1dcc06b7c01e59a606cf4
xavierfav <xavier.favory@gmail.com>
2019-09-17 18:44:44 +0200

    First commit :)

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # percussive-annotator
+
+Start server:
+`python3 start_server.py`
+
+With your browser go to:
+`http://localhost:8100/`
+
+Annotate and save your answers.
---
+++ start_server.py
@@ -0,0 +1,12 @@
+import http.server
+import socketserver
+
+
+PORT = 8100
+
+Handler = http.server.SimpleHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print("serving at port", PORT)
+    httpd.serve_forever()
+    
