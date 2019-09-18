885a591e1bd57fb4ae9e0772dea00c388640309a
xavierfav <xavier.favory@gmail.com>
2019-09-18 13:10:27 +0200

    Add flask backend

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import json
+
+
+app = Flask(__name__, static_url_path='/static')
+
+NUM_SOUNDS_PER_PAGE = 2
+
+
+@app.route('/', methods = ['GET', 'POST'])
+def annotator():
+    if request.method == 'POST':
+        data = request.get_json()
+        return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
+
+    page = int(request.args.get('p', 1))
+    all_sound_ids = json.load(open('static/example_sounds.json'))
+
+    # get a chunk of 20 sounds according to the requested page number
+    sound_ids = all_sound_ids[(page-1)*NUM_SOUNDS_PER_PAGE:page*NUM_SOUNDS_PER_PAGE]
+
+    return render_template("index.html", sound_ids=sound_ids, page=page)
+
+
+if __name__ == '__main__':
+    app.run()
