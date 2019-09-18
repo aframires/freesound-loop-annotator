40afb4fcc2e8612414ab34991a47d8db5cd15e2c
xavierfav <xavier.favory@gmail.com>
2019-09-18 13:20:44 +0200

    Save annotations to json file

--- app.py
+++ app.py
@@ -10,13 +10,15 @@ NUM_SOUNDS_PER_PAGE = 2
 @app.route('/', methods = ['GET', 'POST'])
 def annotator():
     if request.method == 'POST':
+        # save annotations to json file
         data = request.get_json()
+        json.dump(data['answers'], open('annotations/page-{}.json'.format(data['page']), 'w'))
         return json.dumps({'success':True}), 200, {'ContentType':'application/json'} 
 
     page = int(request.args.get('p', 1))
     all_sound_ids = json.load(open('static/example_sounds.json'))
 
-    # get a chunk of 20 sounds according to the requested page number
+    # get a chunk of sounds according to the requested page number
     sound_ids = all_sound_ids[(page-1)*NUM_SOUNDS_PER_PAGE:page*NUM_SOUNDS_PER_PAGE]
 
     return render_template("index.html", sound_ids=sound_ids, page=page)
