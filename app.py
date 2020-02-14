7c6a250a75bad5fb3e07efd6a865e297e5c5dcb4
ffont <frederic.font@upf.edu>
2020-02-14 14:13:47 +0100

    Add basic support for self-assigning sounds

--- app.py
+++ app.py
@@ -1,15 +1,16 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for
 from flask_httpauth import HTTPBasicAuth
 import json
 import os
 import glob
 import errno
+import random
 
 app = Flask(__name__, static_url_path='/fslannotator/static', static_folder='/static/')
 auth = HTTPBasicAuth()
 
 PATH_TO_FSL10K = "/static/FSL10K"
-#PATH_TO_SOUND_IDS = os.path.join(PATH_TO_FSL10K, 'metadata_sound_ids_list.json')
+PATH_TO_ALL_SOUND_IDS = os.path.join(PATH_TO_FSL10K, 'metadata_sound_ids_list.json')
 PATH_TO_SOUND_IDS_PER_USER = os.path.join(PATH_TO_FSL10K, 'metadata_sound_ids_list_username.json')
 PATH_TO_AC_ANALYSIS = os.path.join(PATH_TO_FSL10K, 'ac_analysis/')
 PATH_TO_METADATA = os.path.join(PATH_TO_FSL10K, 'fs_analysis/')
@@ -20,8 +21,13 @@ PATH_TO_USERS_FILE = os.path.join(PATH_TO_FSL10K, 'users.json')
 METRONOME_SOUND_URL = '/fslannotator' + PATH_TO_FSL10K + '/woodblock.wav'
 
 users =  json.load(open(PATH_TO_USERS_FILE, 'rb')) 
-#sound_ids = json.load(open(PATH_TO_SOUND_IDS, 'rb'))
-sound_id_user = json.load(open(PATH_TO_SOUND_IDS_PER_USER, 'rb'))
+all_sound_ids = json.load(open(PATH_TO_ALL_SOUND_IDS, 'rb')) 
+sound_id_user = json.load(open(PATH_TO_SOUND_IDS_PER_USER,'rb'))
+genres_file = json.load(open(PATH_TO_GENRE_FILE, 'rb'))
+
+default_N_assign_more_sounds = 5
+enable_auto_assign_annotations = False
+
 
 @auth.verify_password
 def verify_password(username, password):
@@ -38,10 +44,28 @@ def mkdir_p(path):
         else:
             raise
 
+def assign_more_sounds_to_user(username, N=default_N_assign_more_sounds):
+    # TODO: this function randomly assigns a maximum of N new annotations
+    # we should do a real implementation which choses wisely which sounds
+    # to assign
+    new_ids = random.sample(all_sound_ids, N)
+    current_ids = sound_id_user[username]
+    new_non_overlapping_ids = list(set(new_ids).difference(current_ids))
+    sound_id_user[username] += new_non_overlapping_ids
+    json.dump(sound_id_user, open(PATH_TO_SOUND_IDS_PER_USER, 'w'))
+
+
+@app.route('/fslannotator/assign', methods = ['GET', 'POST'])
+@auth.login_required
+def assign():
+    username = auth.username()
+    assign_more_sounds_to_user(username)
+    return redirect(url_for('annotator'))
+
+
 @app.route('/fslannotator/', methods = ['GET', 'POST'])
 @auth.login_required
 def annotator():
-
     username = auth.username()
     sound_ids_to_annotate = sound_id_user.get(username, None)    
     if sound_ids_to_annotate is None:
@@ -59,7 +83,7 @@ def annotator():
     # get latest annoated sound and corresponding page in sound_ids list
     already_annotated_ids = [str(filename.split('-')[1].split('.')[0]) for filename in os.listdir(
         user_annotations_path) if filename.endswith('.json')]
-    
+
     last_annotated_page = 0
     for sound_id in already_annotated_ids:
         try:
@@ -70,7 +94,10 @@ def annotator():
             continue
 
     page = int(request.args.get('p', last_annotated_page + 1))
-    print('Will annotate page:', page)
+
+    if page > len(sound_ids_to_annotate):
+        # All sounds have been annotated
+        return render_template("finished_annotations.html", N=default_N_assign_more_sounds, enable_auto_assign_annotations=enable_auto_assign_annotations)
 
     # get a chunk of sounds according to the requested page number
     sound_id = sound_ids_to_annotate[(page-1)]
@@ -89,7 +116,6 @@ def annotator():
     base_name = ac_analysis_filename[ac_analysis_filename.rfind("/"):ac_analysis_filename.find("-hq")]
     ac_analysis_filename =  base_name + "_analysis.json"
     ac_analysis = json.load(open(PATH_TO_AC_ANALYSIS + ac_analysis_filename, 'rb'))
-    genres_file = json.load(open(PATH_TO_GENRE_FILE, 'rb'))
     if sound_id in genres_file:
         genres = genres_file[sound_id]
     else:
